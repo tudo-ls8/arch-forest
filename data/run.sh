19092cbb42cfe3ddfaa6812483cb41e6b867533d
Sebastian Buschjäger <sebastian.buschjaeger@tu-dortmund.de>
2018-02-07 19:11:44 +0100

    more little fixes

--- data/run.sh
+++ data/run.sh
@@ -18,7 +18,8 @@ fi
 for d in ./$1/cpp/$2/*/; do
     echo $d
 	cd $d
-	measurments="$d,$(./testStandardIfTree),$(./testOptimizedSwapIfTree),$(./testOptimizedPathIfTree),$(./testOptimizedNodeIfTree),$(./testStandardNativeTree),$(./testOptimizedNativeTree)"
+	#measurments="$d,$(./testStandardIfTree),$(./testOptimizedSwapIfTree),$(./testOptimizedPathIfTree),$(./testOptimizedNodeIfTree),$(./testStandardNativeTree),$(./testOptimizedNativeTree)"
+	measurments="$d,$(./testStandardIfTree),$(./testOptimizedPathIfTree_16000),$(./testOptimizedNodeIfTree_16000),$(./testOptimizedSwapIfTree_16000),$(./testOptimizedPathIfTree_32000),$(./testOptimizedNodeIfTree_32000),$(./testOptimizedSwapIfTree_32000),$(./testOptimizedPathIfTree_64000),$(./testOptimizedNodeIfTree_64000),$(./testOptimizedSwapIfTree_64000),$(./testStandardNativeTree),$(./testOptimizedNativeTree_5),$(./testOptimizedNativeTree_10),$(./testOptimizedNativeTree_25),$(./testOptimizedNativeTree_50)"
 	printf "$measurments \n"
 	cd ../../../..
 	#echo $measurments >> results_$2.csv
--- data/run_all.sh
+++ data/run_all.sh
@@ -6,7 +6,7 @@ then
   exit 1
 fi
 
-echo "config,Mean-StandardPathIfTree,Var-StandardPathIfTree,Mean-StandardSwapIfTree,Var-StandardSwapIfTree,Mean-StandardNodeIfTree,Var-StandardNodeIfTree,Mean-OptimizedIfTree,Var-OptimizedIfTree,Mean-StandardNativeTree,Var-StandardNativeTree,Mean-OptimizedNativeTree,Var-OptimizedNativeTree" > results_$1.txt
+echo "config,Mean-testStandardIfTree,Var-testStandardIfTree,Mean-testOptimizedPathIfTree_16000,Var-testOptimizedPathIfTree_16000,Mean-testOptimizedNodeIfTree_16000,Var-testOptimizedNodeIfTree_16000,Mean-testOptimizedSwapIfTree_16000,Var-testOptimizedSwapIfTree_16000,Mean-testOptimizedPathIfTree_32000,Var-testOptimizedPathIfTree_32000,Mean-testOptimizedNodeIfTree_32000,Var-testOptimizedNodeIfTree_32000,Mean-testOptimizedSwapIfTree_32000,Var-testOptimizedSwapIfTree_32000,Mean-testOptimizedPathIfTree_64000,Var-testOptimizedPathIfTree_64000,Mean-testOptimizedNodeIfTree_64000,Var-testOptimizedNodeIfTree_64000,Mean-testOptimizedSwapIfTree_64000,Var-testOptimizedSwapIfTree_64000,Mean-testStandardNativeTree,Var-testStandardNativeTree,Mean-testOptimizedNativeTree_5,Var-testOptimizedNativeTree_5,Mean-testOptimizedNativeTree_10,Var-testOptimizedNativeTree_10,Mean-testOptimizedNativeTree_25,Var-testOptimizedNativeTree_25,Mean-testOptimizedNativeTree_50,Var-testOptimizedNativeTree_50" > results_$1.txt
 for d in ./*/; do
 	echo "Profiling $d"
 	./run.sh $d $1 >> results_$1.txt
