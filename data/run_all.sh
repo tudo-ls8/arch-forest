e19e75ed0fb38e7a00d093926c8e3f00ca47a78d
Sebastian Buschjäger <sebastian.buschjaeger@tu-dortmund.de>
2018-05-31 13:54:11 +0200

    added plotting script

--- data/run_all.sh
+++ data/run_all.sh
@@ -6,13 +6,19 @@ then
   exit 1
 fi
 
-if [ "$1" == "arm" ] || [ "$1" == "ppc" ]; then
-	echo "config,sklearn,Mean-testStandardIfTree,Var-testStandardIfTree,Filesize-testStandardIfTree,Mean-testOptimizedPathIfTree_32000,Var-testOptimizedPathIfTree_32000,Filesize-testOptimizedPathIfTree_32000,Mean-testOptimizedPathIfTree_64000,Var-testOptimizedPathIfTree_64000,Filesize-testOptimizedPathIfTree_64000,Mean-testStandardNativeTree,Var-testStandardNativeTree,Filesize-testStandardNativeTree,Mean-testOptimizedNativeTree_8,Var-testOptimizedNativeTree_8,Filesize-testOptimizedNativeTree_8,Mean-testOptimizedNativeTree_32,Var-testOptimizedNativeTree_32,Filesize-testOptimizedNativeTree_32" > results_$1.txt
-else
-	echo "config,sklearn,Mean-testStandardIfTree,Var-testStandardIfTree,Filesize-testStandardIfTree,Mean-testOptimizedPathIfTree_128000,Var-testOptimizedPathIfTree_128000,Filesize-testOptimizedPathIfTree_128000,Mean-testOptimizedPathIfTree_384000,Var-testOptimizedPathIfTree_38464000,Filesize-testOptimizedPathIfTree_384000,Mean-testStandardNativeTree,Var-testStandardNativeTree,Filesize-testStandardNativeTree,Mean-testOptimizedNativeTree_25,Var-testOptimizedNativeTree_25,Filesize-testOptimizedNativeTree_25" > results_$1.txt
-fi
+
+echo "path,filename,mean,variance,min,max,size" > results_$1.csv
 
 for d in ./*/; do
+	if [ "$d" != "./__pycache__/" ]; then
+		echo $d/cpp/$1/results.csv
+	fi
+	
+	if [ -f $d/cpp/$1/results.csv ] ; then
+		rm $d/cpp/$1/results.csv
+	fi
+
 	echo "Profiling $d"
-	./run.sh $d $1 >> results_$1.txt
+	./run.sh $d $1
+	cat $d/cpp/$1/results.csv >> results_$1.csv
 done
